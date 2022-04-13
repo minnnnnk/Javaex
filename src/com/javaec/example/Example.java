4e57c0dd3bf2fad634238ceca159490b02b6acc6
y <y@DESKTOP-DMV4N7N>
2022-04-13 17:45:33 +0900

    2022-04-13

---
+++ src/com/javaec/example/Example.java
@@ -0,0 +1,51 @@
+package com.javaec.example;
+
+public class Example {
+	public static void main(String[] args) {
+		int ex1;
+		long ex2;
+		boolean ex3;
+		float ex4;
+		double ex5;
+		
+		ex1 = 1;
+		ex2 = 123049824L;
+		ex3 = false;
+		ex4 = 3.128394f;
+		ex5 = 3.123;
+		System.out.println(ex1);
+		System.out.println(ex2);
+		System.out.println(ex3);
+		System.out.println(ex4);
+		System.out.println(ex5);
+		
+		String number = "01학번";
+		String name = "김민규";
+		boolean isEnrolled = false;
+		int grade = 4 ;
+		String address = "경기도 광명";
+		String major ="자바";
+		double unit = 80.5;
+		boolean haveMinor = false;
+		String juminNo ="9128392138";
+		String cellphone ="1012938129";
+		int age = 25;
+		String email = "juasdhf.naver.com";
+		
+		System.out.println(number);
+		System.out.println(name);
+		System.out.println(isEnrolled);
+		System.out.println(grade);
+		System.out.println(address);
+		System.out.println(major);
+		System.out.println(unit);
+		System.out.println(haveMinor);
+		System.out.println(juminNo);
+		System.out.println(cellphone);
+		System.out.println(age);
+		System.out.println(email);
+		
+		
+		
+	}
+}
